f7b201270b2dd301beeded461349d3844696f4dd
Zack Amis <zachariah.amis@vizientinc.com>
2020-05-01 17:05:07 -0500

    updated code to finished from day1

--- cars.rb
+++ cars.rb
@@ -1,1 +1,25 @@
-puts "Hello World!"
+puts "Let's add a car!"
+
+puts ""
+puts "What is the car's year?"
+year = gets.chomp
+
+puts ""
+puts "What is the car's make?"
+make = gets.chomp
+
+puts ""
+puts "What is the car's model?"
+model = gets.chomp
+
+puts ""
+puts "What is the car's color?"
+color = gets.chomp
+
+puts ""
+puts "What is the car's value?"
+value = gets.chomp
+
+puts ""
+puts "You added a: "
+puts "#{year} #{color} #{make} #{model} worth approximately $#{value}"
