79e8ca824a80eb5a369ccac23318d3649319ab26
Zack Amis <zachariah.amis@vizientinc.com>
2020-06-23 13:59:12 -0500

    updated day5 completion

---
+++ form_data.rb
@@ -0,0 +1,20 @@
+module FormData
+    def self.add_car_questions
+        puts ""
+        puts "Let's add a car!"
+        year = ask_question("year")
+        make = ask_question("make")
+        model = ask_question("model")
+        color = ask_question("color")
+        value = ask_question("value")
+        Car.new(year, make, model, color, value)
+    end
+
+    private
+
+    def self.ask_question(value)
+        puts ""
+        puts "What is the car's #{value}?"
+        gets.chomp
+    end
+end
---
+++ spec/form_data_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../inventory'
+require_relative '../car'
+require_relative '../form_data'
+
+describe FormData do
+
+    before do
+        $stdout = StringIO.new
+    end
+
+    it "add_car_questions should step though adding a new car" do
+        allow(FormData).to receive(:ask_question).with("year").and_return("1991")
+        allow(FormData).to receive(:ask_question).with("make").and_return("toy")
+        allow(FormData).to receive(:ask_question).with("model").and_return("car")
+        allow(FormData).to receive(:ask_question).with("color").and_return("brown")
+        allow(FormData).to receive(:ask_question).with("value").and_return("4")
+        car = FormData.add_car_questions
+        expect(car.year).to eq("1991")
+        expect(car.make).to eq("toy")
+        expect(car.model).to eq("car")
+        expect(car.color).to eq("brown")
+        expect(car.value).to eq("4")
+    end  
+  end
