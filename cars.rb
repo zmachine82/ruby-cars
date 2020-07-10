8a372e6017d07a36130021ecf3d2318fa34c0a1d
Zack Amis <zachariah.amis@vizientinc.com>
2020-07-10 11:06:28 -0500

    update class examples

--- cars.rb
+++ cars.rb
@@ -1,38 +1,32 @@
-def ask_question(value)
-    puts ""
-    puts "What is the car's #{value}?"
-    gets.chomp
-end
-
-def display_car(car)
-    puts ""
-    puts "You added a: "
-    puts car
-end
-
-class Car 
-    attr_reader :year, :make, :model, :color, :value
+class NewCarForm
+    attr_reader :car
     
-    def initialize(year, make, model, color, value)
-        @year = year
-        @make = make
-        @model = model
-        @color = color
-        @value = value
+    def initialize
+        @car = Car.new
+        @car.year = ask_question("year")
+        @car.make = ask_question("make")
+        @car.model = ask_question("model")
+        @car.color = ask_question("color")
+        @car.price = ask_question("price")
     end
-    
+
+    private
+
+    def ask_question(value)
+        puts ""
+        puts "What is the car's #{value}?"
+        gets.chomp
+    end
+end
+
+class Car
+    attr_accessor :year, :make, :model, :color, :price
+
     def to_s
-        "#{year} #{color} #{make} #{model} worth approximately $#{value}"
+        "#{@year} #{@color} #{@make} #{@model} worth approximately $#{@price}"
     end
 end
 
-puts "Let's add a car!"
-year = ask_question("year")
-make = ask_question("make")
-model = ask_question("model")
-color = ask_question("color")
-value = ask_question("value")
-
-display_car(Car.new(year, make, model, color, value))
-
-
+form = NewCarForm.new
+car = form.car
+puts car
